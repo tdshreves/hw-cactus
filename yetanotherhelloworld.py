0035988e20ac616e089f6051adb506715db4b9e9
Tiffany Shreves <tdshreves@gmail.com>
2020-10-21 12:39:45 -1000

    Added exception handling for non-numeric character user input

--- yetanotherhelloworld.py
+++ yetanotherhelloworld.py
@@ -2,14 +2,19 @@
 
 print('This is the Hello World greeter')
 print('How pumped do you want to feel?!')
-enthus_level=input('Very(1)/Normal(2)/Meh(3)/Areyoukiddingit\'s2020(4): ')
-if int(enthus_level) is 1:
-   print('Hello World Heck Yeah!!!')
-if int(enthus_level) is 2:
-   print('Hello World')
-if int(enthus_level) is 3:
-   print('\'sup world')
-if int(enthus_level) is 4:
-   print('goodbye world x(')
-elif int(enthus_level) not in [1,2,3,4]:
-   print('There\'s nothing I can do for you, bye')
+while True:
+   try:
+        enthus_level=input('Very(0)/Normal(2)/Meh(3)/Areyoukiddingit\'s2020(4): ')
+        if int(enthus_level) is 1:
+           print('Hello World Heck Yeah!!!')
+        if int(enthus_level) is 2:
+           print('Hello World')
+        if int(enthus_level) is 3:
+           print('\'sup world')
+        if int(enthus_level) is 4:
+           print('goodbye world x(')
+        elif int(enthus_level) not in [1,2,3,4]:
+           print('There\'s nothing I can do for you, bye')
+        break
+   except ValueError:
+        print('The option has to be an integer')
