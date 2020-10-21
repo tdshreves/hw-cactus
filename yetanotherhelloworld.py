048e445dfccb33849b0a13b30c4ad87d2bf0684f
Tiffany Shreves <tdshreves@gmail.com>
2020-10-21 11:55:08 -1000

    Added options so the user can feel as enthused or unenthused as they want

--- yetanotherhelloworld.py
+++ yetanotherhelloworld.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
-print('Hello world!')
+print('This is the Hello World greeter')
+print('How pumped do you want to feel?!')
+enthus_level=input('Very(1)/Normal(2)/Meh(3)/Areyoukiddingit\'s2020(4): ')
+if int(enthus_level) is 1:
+   print('Hello World Heck Yeah!!!')
+if int(enthus_level) is 2:
+   print('Hello World')
+if int(enthus_level) is 3:
+   print('\'sup world')
+if int(enthus_level) is 4:
+   print('goodbye world x(')
